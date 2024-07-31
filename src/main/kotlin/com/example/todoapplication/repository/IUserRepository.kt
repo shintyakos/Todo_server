42c7da12c61afb2e2bb257cada9b0215b34fa270
shintyakos <shintyakos1145@gmail.com>
2024-07-31 23:18:17 +0900

    ログイン認証を追加

--- src/main/kotlin/com/example/todoapplication/repository/IUserRepository.kt
+++ src/main/kotlin/com/example/todoapplication/repository/IUserRepository.kt
@@ -2,8 +2,13 @@ package com.example.todoapplication.repository
 
 import com.example.todoapplication.entity.Users
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface IUserRepository: JpaRepository<Users, UUID>
+interface IUserRepository: JpaRepository<Users, UUID> {
+    @Query(value = "SELECT * FROM users WHERE users.username = :username", nativeQuery = true)
+    fun findByUserName(@Param("username") username: String): Users
+}
--- src/main/kotlin/com/example/todoapplication/services/UserService.kt
+++ src/main/kotlin/com/example/todoapplication/services/UserService.kt
@@ -1,14 +1,18 @@
 package com.example.todoapplication.services
 
+import com.example.todoapplication.model.MyUserDetails
 import com.example.todoapplication.model.UserData
 import com.example.todoapplication.repository.IUserRepository
 import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
 @Transactional
-class UserService {
+class UserService: UserDetailsService {
     @Autowired
     private lateinit var userRepository: IUserRepository
 
@@ -21,5 +25,18 @@ class UserService {
                 email = it.email
             )
         }
+    }
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val user = userRepository.findByUserName(username ?: "")
+        println("User: $user")
+        val userDetails = MyUserDetails(
+            id = user.id,
+            username = user.name,
+            password = user.password,
+            email = user.email,
+            authorities = AuthorityUtils.createAuthorityList("ROLE_USER")
+        )
+        return userDetails
     }
 }
